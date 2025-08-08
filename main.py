5879b4dccea3bcfa276eda012e3abf5111f55eec
Paul Vanden Broeck <PVB96P@gmail.com>
2025-08-07 22:44:29 -0400

    adding chat history to cli prompt version

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
     
 def chat_session():
 	#while user input is not "Exit"
+	chat_messages = []
 	while True:
 		user_input = input("You: ")
 		if user_input.lower() == "exit":
 			break
 		chat_request = ollama_service.build_chat_request(user_input)
+		chat_messages.append(chat_request.messages[0])
+		chat_request.messages = chat_messages
 		response = ollama_service.get_chat_response(chat_request)
+		chat_messages.append(response.message)
 		response_text = parse_chat_response(response)
 		print(f"Bot: {response_text}")
 
